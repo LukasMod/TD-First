f51df72cf24e7f1b9c669b0ac254c096a724678f
LukasMod <lukasz.adam.modzelewski@gmail.com>
2023-11-10 12:43:41 +0100

    git init, spawner, enemies

---
+++ Assets/Scripts/Enemy/EnemyHealthContainer.cs
@@ -0,0 +1,9 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthContainer : MonoBehaviour
+{
+    [SerializeField] private Image fillAmountImage;
+    public Image FillAmountImage => fillAmountImage;
+}
